0768a1a291abdb41e75da84e59c7e4064b6b379c
Stephen Wastling <stephen.wastling@nhs.net>
2022-01-23 19:57:21 +0000

    Remove references to nibabel versions in tests/test_config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,7 +3,5 @@ import musclemap.config as config
 
 def test_config():
 
-    assert isinstance(config.NIBABEL_VERSIONS, list)
-    assert isinstance(config.NIBABEL_VERSIONS[0], str)
     assert isinstance(config.FSL_VERSIONS, list)
     assert isinstance(config.FSL_VERSIONS[0], str)
